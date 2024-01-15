eabe5b6599b43b6051feeec419ee6ea5e71a2546
nono <arnaudb92230@gmail.com>
2024-01-15 14:02:29 +0100

    WIP: add power on calculate

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 class MathOperations :
 
     def addition(self, a : float, b : float):
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            raise ValueError("Veuillez entrer des nombres valides")
+
         return a + b
     
     def substraction(self, a : float, b : float):
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            raise ValueError("Veuillez entrer des nombres valides")
+
         return a - b
     
     def multiplication(self,a : float, b : float):
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            raise ValueError("Veuillez entrer des nombres valides")
+
         return a * b
     
     def division(self, a : float, b : float):
-        return a /b
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            raise ValueError("Veuillez entrer des nombres valides")
+        else:
+            if b != 0:
+                return a / b
+            else:
+                raise ValueError("Division par zéro impossible")
+            
+    def power(self, a : float, b : float):
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            raise ValueError("Veuillez entrer des nombres valides")
+        if a < 0 and not isinstance(b, (int)):
+            raise ValueError("Pas de puissances non entière pour les nombres négatifs")
+        else:
+            return a ** b
--- test.py
+++ test.py
@@ -7,18 +7,23 @@ def test_addition_with_parametrize(a,b, expected):
     x = MathOperations()
     assert x.addition(a,b) == expected
 
-@pytest.mark.parametrize("a, b, expected", [(2,1,1),(-1,-2,-3),(10.5,10.5, 0)])
+@pytest.mark.parametrize("a, b, expected", [(2,1,1),(-1,-2,1),(10.5,10.5, 0)])
 def test_substraction_with_parametrize(a,b, expected):
     x = MathOperations()
     assert x.substraction(a,b) == expected
 
-@pytest.mark.parametrize("a, b, expected", [(2,2,4),(-6,-2,-12),(10.5,10.5,110.25)])
+@pytest.mark.parametrize("a, b, expected", [(2,2,4),(-6,-2,12),(10.5,10.5,110.25)])
 def test_multiplication_with_parametrize(a,b, expected):
     x = MathOperations()
     assert x.multiplication(a,b) == expected
 
-@pytest.mark.parametrize("a, b, expected", [(6,2,3),(-40,-2,-20),(10.5,3.4,2.333)])
+@pytest.mark.parametrize("a, b, expected", [(6,2,3),(-40,-2,20),(10.5,2.5,4.2)])
 def test_division_with_parametrize(a,b, expected):
     x = MathOperations()
     assert x.division(a,b) == expected
+
+@pytest.mark.parametrize("a, b, expected", [(2,1,2),(-5,3,-125),(2,0,1), (9,0.5,3)])
+def test_power_with_parametrize(a,b, expected):
+    x = MathOperations()
+    assert x.power(a,b) == expected
 
