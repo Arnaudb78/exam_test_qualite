0536825adcbcb5b1426578f780df83d855701f08
nono <arnaudb92230@gmail.com>
2024-01-18 20:10:17 +0100

    WIP : add xmml file coverage

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import pytest
 from main import MathOperations
 
 
-@pytest.mark.parametrize("a, b, expected", [(1,2,3),(-1,-2,-3),(10.5,10.5,21)])
+@pytest.mark.parametrize("a, b, expected", [(1,2,3),(-1,-2,-3),(10.5,10.5,21),('chat','chien',None)])
 def test_addition_with_parametrize(a,b, expected):
     x = MathOperations()
     assert x.addition(a,b) == expected
